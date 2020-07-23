38e1c790625b509056b9af4e439150c13843dbfe
Cesar Escobar <cesarsilvaescobar@gmail.com>
2020-07-23 17:17:09 -0300

    [1:07:32] tempo de aula

---
+++ clone-twitter/src/components/Main/index.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import ProfilePage from '../ProfilePage';
+
+import {
+  Container,
+  Header,
+  BackIcon,
+  ProfileIcon,
+  BottomMenu,
+  HomeIcon,
+  SearchIcon,
+  BellIcon,
+  EmailIcon,
+} from './styles';
+
+const Main: React.FC = () => {
+  return (
+    <Container>
+      <Header>
+        <button>
+          <BackIcon />
+        </button>
+        <ProfileIcon>
+          <strong>Guilherme Rodz</strong>
+          <span> 612 Tweets</span>
+        </ProfileIcon>
+      </Header>
+      <ProfilePage />
+
+      <BottomMenu>
+        <HomeIcon className="active" />
+        <SearchIcon />
+        <BellIcon />
+        <EmailIcon />
+      </BottomMenu>
+    </Container>
+  );
+};
+
+export default Main;
---
+++ clone-twitter/src/components/ProfilePage/index.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+
+import {
+  Container,
+  Banner,
+  Avatar,
+  ProfileData,
+  LocationIcon,
+  CakeIcon,
+  Followage,
+  EditButton,
+} from './styles';
+
+const ProfilePage: React.FC = () => {
+  return (
+    <Container>
+      <Banner>
+        <Avatar />
+      </Banner>
+      <ProfileData>
+        <EditButton outlined>Editar perfil</EditButton>
+        <h1>Guilherme Rodz</h1>
+        <h2>@guilherme_rodz</h2>
+
+        <p>
+          Developer at <a href="https://rocketseat.com.br/">@RocketSeat</a>
+        </p>
+
+        <ul>
+          <li>
+            <LocationIcon />
+            São Paulo, Brasil
+          </li>
+          <li>
+            <CakeIcon />
+            Nascido(a) em 21 de Junho 2001
+          </li>
+        </ul>
+
+        <Followage>
+          <span>
+            seguindo <strong>94</strong>
+          </span>
+          <span>
+            <strong>672 </strong>seguidores
+          </span>
+        </Followage>
+      </ProfileData>
+      {/* <Feed></Feed> */}
+    </Container>
+  );
+};
+
+export default ProfilePage;
